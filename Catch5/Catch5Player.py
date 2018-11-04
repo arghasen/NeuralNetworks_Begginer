6b8dab1f7ff6ef32a7e943a0a1006daca901745e
Argha Sen <iamslowdeath@gmail.com>
2018-11-04 18:45:27 +0530

    move helper classes to separate file

--- Catch5/Catch5Player.py
+++ Catch5/Catch5Player.py
@@ -1,27 +1,7 @@
 import random
 import Catch5
 import numpy as np
-
-ActionMap = {0: 3, 1: 2, 2: 1, 3: -1, 4: -2, 5: -3}
-
-
-class TrainStats():
-    def __init__(self):
-        self.epoch = 0
-        self.nb_wins = 0
-        self.nb_losses = 0
-        self.p_wins = 0
-        self.p_loss = 0
-
-
-class AgentConfig():
-    def __init__(self):
-        self.nb_epoch = 10000
-        self.print_every_n_epoch = 1
-        self.plot_charts = True
-        self.discount_factor = 0.99
-        self.initial_randomness_rate = 1
-
+from helpers import ActionMap, AgentConfig, TrainStats
 
 class Agent:
     def __init__(self, config):
