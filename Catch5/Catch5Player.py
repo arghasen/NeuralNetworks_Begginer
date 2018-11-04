d1f543ad9e83f5d6ea41c3dd810091a52b496244
Argha Sen <iamslowdeath@gmail.com>
2018-11-04 18:00:05 +0530

    create the agent class

--- Catch5/Catch5Player.py
+++ Catch5/Catch5Player.py
@@ -2,48 +2,91 @@ import random
 import Catch5
 
 ActionMap = {0: 3, 1: 2, 2: 1, 3: -1, 4: -2, 5: -3}
-
-# creating random actions
-
-
-def get_action():
-    return random.randrange(0, 6)
-
-# mapping actions (0, 1, 2, 3, 4, 5) to answers (3, 2, 1, -1 , -2, -3)
-
-
-def action_to_answer(action):
-
-    return ActionMap[action]
+class TrainStats():
+    def __init__(self):
+        self.epoch  = 0
+        self.nb_wins = 0
+        self.nb_losses = 0
+        self.p_wins = 0
+        self.p_loss = 0
 
 
-def play(num_times):
-    game = Catch5.Game()
-    won = 0
-    for nb_game in range(1, num_times + 1):
+class Agent:
+    def __init__(self):
+        self.game = Catch5.Game()
 
-        game.reset()
+    def get_action(self, state):
+        return random.randint(0,5)
+
+    # mapping actions (0..5) to answers (3..-3)
+    def action_to_answer(self, action):
+        return ActionMap[action]
+
+    def print_epoch_stats(self, stats):
+        print('Epoch: {stats.epoch} Wins: {stats.nb_wins} ({stats.p_wins:.2f}%) Losses: {stats.nb_losses} ({stats.p_loss:.2f}%)'.format(stats=stats))
+
+    def play(self, num_times):
+
+      for nb_game in range(1, num_times + 1):
+        self.game.reset()
         print('Starting game #{nb_game}'.format(nb_game=nb_game))
-        while (game.is_active()):
-            print('Current number is {current_number}'.format(
-                current_number=game.current_number))
-            action = get_action()
-            human_readable_answer = action_to_answer(action)
-
+        while (self.game.is_active()):
+            print('Current number is {current_number}'.format(current_number=self.game.current_number))
+            action = self.get_action(self.game.current_number)
+            human_readable_answer = self.action_to_answer(action)
+            
             print('Playing {answer}'.format(answer=human_readable_answer))
-            game.play(human_readable_answer)
+            self.game.play(human_readable_answer)
 
-            if (game.has_won()):
+            if (self.game.has_won()):
                 print('You won!')
-                won = won + 1
 
-            if (game.has_lost()):
+            if (self.game.has_lost()):
                 print('You lost')
 
         print('##############################################')
-        print("stats:")
-        print("Won ",won)
-        print("Won % ",won/num_times)
+
+    def get_reward(self):
+        if self.game.has_won():
+            return 1
+        elif self.game.has_lost():
+            return -1
+        else:
+            return -0.1
+
+    def play_and_train(self):
+        stats = TrainStats()
+
+        for epoch in range(1, 101):
+
+            self.game.reset()
+            stats.epoch = epoch
+
+            while (self.game.is_active()):
+
+                state = self.game.current_number
+
+                action = self.get_action(state)
+                human_readable_answer = self.action_to_answer(action)
+
+                self.game.play(human_readable_answer)
+
+                reward = self.get_reward()
+                next_state = self.game.current_number
+                final = not self.game.is_active()
+                
+
+                if (self.game.has_won()):
+                    stats.nb_wins += 1
+
+                if (self.game.has_lost()):
+                    stats.nb_losses += 1
+
+                stats.p_wins = 100 / epoch * stats.nb_wins
+                stats.p_loss = 100 / epoch * stats.nb_losses
+
+            if (epoch  == 100):
+                self.print_epoch_stats(stats)
 
 
-play(100)
+Agent().play_and_train()
---
+++ Catch5/Results.md
@@ -0,0 +1,4 @@
+# Catch5
+The game is always winnable
+
+A random player won about **24%** of the games. It was suprising to me as I didnt expect it to take 1/2/3 steps in correct order. But as there are multiple ways of winning the same game as from 9 to 5 we can go via -1/-3, -2/-2 and -3/-1 this helps random play to win so much.
